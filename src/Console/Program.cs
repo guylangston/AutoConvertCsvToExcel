1761cb621163a55cb01bb6da2d620fade90ba3bf
Guy Langston <guylangston@gmail.com>
2022-01-03 19:37:27 +0000

    Misc

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,22 +1,21 @@
 ﻿if (args.Length == 0)
 {
-   Console.Error.WriteLine("No files given. Exiting");
-   return -1;
+    Console.Error.WriteLine("No files given. Exiting");
+    return -1;
 }
 
 var inp = args[0];
 if (!string.Equals(Path.GetExtension(inp), ".csv", StringComparison.InvariantCultureIgnoreCase))
 {
-   Console.Error.WriteLine("Input file is not .CSV");
-   return -2;
+    Console.Error.WriteLine("Input file is not .CSV");
+    return -2;
 }
 
 if (!File.Exists(inp))
 {
-   Console.Error.WriteLine("Input File does not exit. Exiting...");
-   return -3;
+    Console.Error.WriteLine("Input File does not exit. Exiting...");
+    return -3;
 }
-
 
 var info = new FileInfo(inp);
 Console.WriteLine($"Input File: {info.FullName}");
@@ -30,15 +29,16 @@ var cellCount = 0;
 var reader = new SerialReaderCSV(readerTxt);
 foreach (var row in reader.ForEachRow())
 {
-   var colIdx = 0;
-   foreach(var cell in row)
-   {
-      cellCount++;
-      colIdx++;
-   }
+    var colIdx = 0;
+    foreach (var cell in row)
+    {
+        Console.WriteLine(cell);
+        cellCount++;
+        colIdx++;
+    }
+
     rowIdx++;
 }
 
-Console.WriteLine($"Lines Read: {rowIdx}; Total Cells: {cellCount}");
-
+Console.WriteLine($"Rows Read: {rowIdx}; Total Cells: {cellCount}");
 return 0;
