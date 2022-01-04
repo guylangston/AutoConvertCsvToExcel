79ca7b9f878ae2d35e788853d4a0ef1c9f53ddc6
Guy Langston <guylangston@hotmail.com>
2022-01-04 11:46:23 +0000

    Working Version

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -18,9 +18,36 @@ if (!File.Exists(inp))
 }
 
 var info = new FileInfo(inp);
-Console.WriteLine($"Input File: {info.FullName}");
+Console.WriteLine($"  [Input] {info.FullName}");
+
+var outFilePath = Path.Combine(info.DirectoryName, Path.GetFileNameWithoutExtension(info.Name) + ".xlsx");
+Console.WriteLine($" [Output] {outFilePath}");
+
+if (File.Exists(outFilePath))
+{
+    if (args.Any(x=>x.Equals("-f", StringComparison.CurrentCultureIgnoreCase)))
+    {
+        Console.WriteLine($"WARNING: Overwiting {outFilePath}");
+    }
+    else
+    {
+        Console.Error.WriteLine("Output file already exists... Stopping");
+        return -4;
+    }
+    
+}
 
 // Create new workbook
+var wb = new NPOI.XSSF.UserModel.XSSFWorkbook();
+var sheet = wb.CreateSheet();
+
+var dataFormatCustom = wb.CreateDataFormat();
+var styleDate = wb.CreateCellStyle();
+styleDate.DataFormat = dataFormatCustom.GetFormat("yyyy-MM-dd");
+
+var styleDateTime = wb.CreateCellStyle();
+styleDateTime.DataFormat = dataFormatCustom.GetFormat("yyyy-MM-dd hh:mm:ss");
+
 
 // Read Input
 var readerTxt = File.OpenText(inp);
@@ -30,9 +57,30 @@ var reader = new SerialReaderCSV(readerTxt);
 foreach (var row in reader.ForEachRow())
 {
     var colIdx = 0;
+
+    var outRow = sheet.CreateRow(rowIdx);
     foreach (var cell in row)
     {
-        Console.WriteLine(cell);
+        var outCell = outRow.CreateCell(colIdx);
+
+        if (DateTime.TryParse(cell, out var dt))
+        {
+            outCell.SetCellValue(dt);
+            outCell.CellStyle = (dt.TimeOfDay.TotalSeconds == 0)
+             ? styleDate
+             : styleDateTime;
+
+        }
+        else if (double.TryParse(cell, out var num))
+        {
+            outCell.SetCellValue(num);
+        }
+        else
+        {
+            outCell.SetCellValue(cell);
+        }
+        
+
         cellCount++;
         colIdx++;
     }
@@ -40,5 +88,16 @@ foreach (var row in reader.ForEachRow())
     rowIdx++;
 }
 
-Console.WriteLine($"Rows Read: {rowIdx}; Total Cells: {cellCount}");
+
+using var outFile = File.Create(outFilePath);
+wb.Write(outFile);
+
+Console.WriteLine($"[Convert] Rows: {rowIdx}; Cells: {cellCount}");
+
+Console.WriteLine($" [Launch] Excel (or associated program from .XLSX)");
+System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+{
+    UseShellExecute = true,
+    FileName = outFilePath
+});
 return 0;
